a266a84af8783c744f4bb51bcdd3e48443fd50fc
Luan <luan_saopaulo06@hotmail.com>
2019-10-08 20:28:35 -0300

    update

--- Avançado/javascript-avancado-i-aula1/aluraframe/client/js/controllers/NegociacaoController.js
+++ Avançado/javascript-avancado-i-aula1/aluraframe/client/js/controllers/NegociacaoController.js
@@ -4,15 +4,34 @@ class NegociacaoController{
 
         let $ = document.querySelector.bind(document)
 
-        this._data = $('#data')
-        this._quantidade = $('#quantidade')
-        this._valor = $('valor')
+        this._input_data = $('#data')
+        this._input_quantidade = $('#quantidade')
+        this._input_valor = $('#valor')
 
     }
 
     adiciona(event){
         event.preventDefault()
-        alert('Chamei o método adiciona')
+        
+        console.log(this._input_data.value)
+
+        let myDate = new Date(...
+            this._input_data.value.split('-').map((element, index) => element - index % 2)
+        )
+        
+        console.log(myDate)
+
+        let negociacao = new Negociacao(
+            myDate,
+            this._input_quantidade.value,
+            this._input_valor.value
+        )
+        
     }
+
+    incrementar(){
+        let incremento = document.querySelector("#incrementado")
+        incremento.innerHTML ++
+    }   
 
 }
